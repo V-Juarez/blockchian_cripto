807bab52c2a0acaf2134abf8ad6835001a1af893
victoriano <mytacam78@gmail.com>
2023-01-13 12:14:27 -0600

    build(game) estructura narrativa y opciones rust

--- basic_rust/platzi-rust/src/main.rs
+++ basic_rust/platzi-rust/src/main.rs
@@ -1,60 +1,58 @@
-use regex::Regex;
+use csv::{ReaderBuilder, StringRecord};
+use std::collections::{HashMap};
+use std::{fs};
+
+
+const FILENAME: &str = "../history.csv";
+
+// Tipo, Tag, Texto, vida
+
+#[derive(Debug)]
+struct DatoHistoria {
+    tipo_dato: String,
+    tag: String,
+    texto: String,
+    vida: i32
+}
+
+impl DatoHistoria {
+    fn new(row: StringRecord) -> DatoHistoria{
+        let vida = row.get(3).unwrap().trim();
+        let vida : i32 = vida.parse().unwrap_or(0);
+
+        return DatoHistoria {
+            tipo_dato: row.get(0).unwrap().trim().to_string(),
+            tag: row.get(1).unwrap().trim().to_string(),
+            texto: row.get(2).unwrap().trim().to_string(),
+            vida: vida,
+        };
+    }
+}
 
 fn main() {
-    println!("Hola rust and cargo");
 
-    // Regex
-    let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
-    let re_mult = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+    let mut datos_historia : HashMap<String, DatoHistoria> = HashMap::new();
 
-    // Traer datos del usuario
-    println!("Introduce tu expression: ");
-    let mut expression = String::new();
-    std::io::stdin().read_line(&mut expression).unwrap();
+    let content = fs::read_to_string(FILENAME).unwrap();
+    // println!("{}", content);
 
-    // Multiplicacion
-    loop {
-        // Aplicar Operaciones
-        let caps = re_mult.captures(expression.as_str());
+    let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
 
-        if caps.is_none() {
-            break;
-        }
+    for result in rdr.records() {
+        let result = result.unwrap();
+        let dato = DatoHistoria::new(result);
+        datos_historia.insert(dato.tag.clone(), dato);
 
-        let caps = caps.unwrap();
-
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value : i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value : i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-        let mult = left_value * right_value;
-
-        expression = expression.replace(cap_expression, &mult.to_string());
-        // println!("{:?} izq: {}, der: {}", caps, left_value, right_value);
+        // println!("Texto: {}", result.unwrap().get(2).unwrap().trim());
     }
 
-    // Suma
-    loop {
-        // Aplicar Operaciones
-        let caps = re_add.captures(expression.as_str());
+    println!("{:?}", datos_historia["DERECHA"]);
 
-        if caps.is_none() {
-            break;
-        }
-
-        let caps = caps.unwrap();
-
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value : i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value : i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-        let addition = left_value + right_value;
-
-        expression = expression.replace(cap_expression, &addition.to_string());
-        // println!("{:?} izq: {}, der: {}", caps, left_value, right_value);
-    }
-
-
-    // Mostrar resultado
-    println!("Resultado: {}", expression);
+    // let mut diccionario : HashMap<String, String> = HashMap::new();
+    // diccionario.insert("Manzana".to_string(), "Es una fruta de color rojo".to_string());
+    // diccionario.insert("Pera".to_string(), "Es una fruta de color verder".to_string());
+    //
+    // println!("La descriptcion de manzana es: {}", diccionario["Manzana"]);
+    // println!("La descriptcion de manzana es: {}", diccionario["Pera"]);
 }
+
